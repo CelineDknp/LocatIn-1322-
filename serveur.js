d5d256b7585e83a68d0e3f1c31f63062241e866c
Céline Deknop <Cline Deknop>
2016-05-21 19:43:40 +0200

    test Heroku 3

--- serveur.js
+++ serveur.js
@@ -10,6 +10,82 @@ var engines = require('consolidate');//Pour utiliser ejs pour les pages dynamiqu
 var cookieParser = require('cookie-parser');//Des cookies !
 var session = require('express-session');//Des sessions
 
+var debug = require('debug')('test:server');
+var http = require('http');
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
 //Les chemins où trouver les fichiers statiques
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('public/styles', express.static(path.join(__dirname, 'public/style')));
@@ -55,6 +131,6 @@ app.use(function(req, res, next){
 
 });
 
-app.listen(8080);
+//app.listen(8080);
 
 console.log("Running at Port 8080");
